8a0ffc1fd199f27ea24fd614a22ac6eca5eafb81
Jose Alejandro Lopez Flores <aleflo_1996@outlook.com>
2020-08-20 13:04:56 -0500

    ADD: Agrega sonido al scanner

--- js/lector.js
+++ js/lector.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const $resultados = document.querySelector("#precio");
     const initLector = () => {
         console.log("entraaaa");
         Quagga.init({
@@ -25,43 +24,14 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     
         Quagga.onDetected((data) => {
+            sound.play();
             app.codigoActual = data.codeResult.code;
             app.setDatosActuales(data.codeResult.code);
-            // Imprimimos todo el data para que puedas depurar
-            // console.log(data);
             Quagga.stop();
+            window.navigator.vibrate(200);
             app.setScannerVisible(false);
-            console.log("stoped", app.codigoActual);
         });
     
-        // Quagga.onProcessed(function (result) {
-        //     var drawingCtx = Quagga.canvas.ctx.overlay,
-        //         drawingCanvas = Quagga.canvas.dom.overlay;
-    
-        //     if (result) {
-        //         if (result.boxes) {
-        //             drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
-        //             result.boxes.filter(function (box) {
-        //                 return box !== result.box;
-        //             }).forEach(function (box) {
-        //                 Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, { color: "green", lineWidth: 2 });
-        //             });
-        //         }
-    
-        //         if (result.box) {
-        //             Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, { color: "#00F", lineWidth: 2 });
-        //         }
-    
-        //         if (result.codeResult && result.codeResult.code) {
-        //             Quagga.ImageDebug.drawPath(result.line, { x: 'x', y: 'y' }, drawingCtx, { color: 'red', lineWidth: 3 });
-        //         }
-        //     }
-        // });
     }
     objectQuagga.initQuagga = initLector;
-    // (any) => {
-    //     console.log("Nuevo "+any);
-    // };
-    // console.log(initLector());
-    // initQuagga(false);
 });
