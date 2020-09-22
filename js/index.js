ce841c36220fedc8be465c8d878f1276b7e91208
Jose Alejandro Lopez Flores <aleflo_1996@outlook.com>
2020-09-21 19:52:42 -0500

    ADD: Implementa axios para consultas a la api

--- js/index.js
+++ js/index.js
@@ -11,31 +11,47 @@ var app = new Vue({
             {codigo: "3800065711135", name: "Pasta de dientes Colgate", precio: "19.90", descripcion: "Pasta dental triple accion"},
             {codigo: "9382298282288", name: "Cocacola", precio: "14.50", descripcion: "Refresco no retornable"}
         ],
-        imageCode: false
+        imageCode: false,
+        urlApi: "https://....."
     },
     methods: {
         activateScanner: function() {
             this.scannerVisible = true;
             objectQuagga.initQuagga();
-            this.startLoading(500);
         },
         stopScanner: function() {
             Quagga.stop();
             this.scannerVisible = false;
         },
         setDatosActuales: function(codigo) {
+            this.startLoading(500);
+            const instancia = this;
             this.codigoActual = codigo;
-            const productFind = this.productos.filter((producto) => producto.codigo == codigo);
-            if (productFind.length === 0) {
-                this.name = "Producto no encontrado";
-                this.precio = "00.00";
-                this.description = "Intente de nuevo";
-            } else {
-                this.name = productFind[0].name;
-                this.precio = productFind[0].precio;
-                this.description = productFind[0].descripcion;
-            }
-            app.imageCode = true;
+
+            const uriComplete = `${this.urlApi}${codigo}/precios`;
+
+            axios.get(uriComplete)
+            .then(function (response) {
+                const respons = response.data.data;
+                if (respons.length === 0) {
+                    instancia.name = "Producto no encontrado";
+                    instancia.precio = "00.00";
+                    instancia.description = "Intente de nuevo";
+                } else {
+                    instancia.name = respons.nombre;
+                    instancia.precio = respons.Precio1IVAUV;
+                    instancia.description = respons.descripcion;
+                }
+                app.imageCode = true;
+                instancia.stopLoading(500);
+            })
+            .catch(function (error) {
+                instancia.name = "Fallo en la conexion";
+                instancia.precio = "00.00";
+                instancia.description = "Intente de nuevo";
+                instancia.stopLoading(500);
+                console.log('Error: ' + error);
+            });
         },
         setScannerVisible: function(visible) {
             this.scannerVisible = visible;
